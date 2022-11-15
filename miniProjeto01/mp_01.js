b8c77022169f9e047b39a2f30c6f4fc78e2d83e1
borges18 <mateusbmsilva@gmail.com>
2022-11-14 23:32:00 -0300

    Mini Projeto 01

---
+++ miniProjeto01/mp_01.js
@@ -0,0 +1,14 @@
+// Objetivo do desafio é fazer a troca de variáveis, entre a variável "a" e "b"
+
+let a = "vermelho";
+let b = "azul";
+
+// troca do valor da variável
+let c = a;
+
+a = b;
+b = c;
+
+
+console.log(a);
+console.log(b);
